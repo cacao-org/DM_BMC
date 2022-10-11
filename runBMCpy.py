4b5b223de2149cbe6e2a505ba9d4f707a2a41f47
first@kamua <first@kamua>
2022-10-11 08:34:31 +0000

    [from seb-dev] seb dump

--- runBMCpy.py
+++ runBMCpy.py
@@ -11,27 +11,57 @@ dm_command = np.zeros(
 # NOTES : tip/tilt values in radians. [min;max] = [-1e-3 ; 1e-3]
 #         piston values in nanometers
 
-# dm_command[3*3+2] = -1.e-3
-# dm_command[3*3+1] = -1.e-3
-# dm_command[3*33+2] = -1.e-3
-# dm_command[3*33+1] = -1.e-3
+# dm_command[3*3] = -2000
+# dm_command[3*3+2] = -2.e-3
+# dm_command[3*3+1] = -2.e-3
+
+# dm_command[3*33] = -2000
+# dm_command[3*33+2] = -2.e-3
+# dm_command[3*33+1] = -2.e-3
+
+# dm_command[3*2] = -2000
+# dm_command[3*2+2] = 2.e-3
+# dm_command[3*2+1] = 2.e-3
+
+# dm_command[3*29] = -2000
+# dm_command[3*29+2] = -2.e-3
+# dm_command[3*29+1] = -3.e-3
+
+
+# dm_command[3*30] = -2000
+# dm_command[3*30+2] = -1.e-3
+# dm_command[3*30+1] = -2.e-3
+
 
 # dm_command[3*21] = -2000
-# dm_command[3*21+2] = -2.e-3
-# dm_command[3*21+1] = -2.e-3
+# dm_command[3*21+2] = 1e-3
+# dm_command[3*21+1] = 0.5e-3
+
+# dm_command[3*34] = -2000
+# dm_command[3*34+2] = -2e-3
+# dm_command[3*34+1] = -2e-3
+
+# dm_command[3*2] = -2000
+# dm_command[3*2+2] = 2e-3
+# dm_command[3*2+1] = -2e-3
+
 # dmptt.set_data(dm_command.astype(np.float64))
 
-for k in range(37):
-    # dm_command  = np.zeros(111, dtype=np.float64)
-    dm_command[k * 3] = -2000
-    dm_command[k * 3 + 2] = 2.e-3
-    dm_command[k * 3 + 1] = 2.e-3
+## ALL Tilted
+# for k in range(37):
+#     dm_command  = np.zeros(111, dtype=np.float64)
+#     dm_command[k*3] = -2000
+#     dm_command[k*3+2] = 2.e-3
+#     dm_command[k*3+1] = 2.e-3
+#     dmptt.set_data(dm_command.astype(np.float64))
+#     time.sleep(0.5)
+#     print(k)
 
 # for i in np.arange(-3,3):
 #     for j in np.arange(-3,3):
-#         dm_command[29*3] = -2000
-#         dm_command[29*3+2] = i*1e-3
-#         dm_command[29*3+1] = j*1e-3
+#         dm_command[30*3] = -2000
+#         dm_command[30*3+2] = i*1e-3
+#         dm_command[30*3+1] = j*1e-3
 #         dmptt.set_data(dm_command.astype(np.float64))
 #         time.sleep(0.5)
 #         print(i,j)
