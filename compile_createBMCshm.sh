d2896c1264dcdd7c6a01380b02a045c96892d0f8
vdeo <vdeo@naoj.org>
2022-11-01 10:47:16 -1000

    Clean compilefiles - not checked

--- compile_createBMCshm.sh
+++ compile_createBMCshm.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-IFLAGS="-I/opt/Boston/include/ -I/usr/local/milk/include/ImageStreamIO"
-LFLAGS="-L/opt/Boston/lib/ -L/usr/local/milk/lib/"
+if [ -z "$MILK_ROOT" ]; then
+    echo "MILK_ROOT env variable undefined! Aborting."
+    exit 1
+fi
+ISIO_ROOT=${MILK_ROOT}/src/ImageStreamIO
+
+if [ -z "$MILK_INSTALLDIR" ]; then
+    echo "MILK_INSTALLDIR env variable undefined! Aborting."
+    exit 1
+fi
+
+BMC_DIR="/opt/Boston"
+
+IFLAGS="-I${BMC_DIR}/include/ -I${ISIO_ROOT}"
+LFLAGS="-L${BMC_DIR}/lib/ -L${MILK_INSTALLDIR}/lib/"
 lFLAGS="-lBMC -lBMC_PCIeAPI -lBMC_USBAPI -lImageStreamIO -lBMC_OpenLoop -lpthread"
-OTHFLAGS="-Wl,-rpath=/usr/local/milk/lib:/opt/Boston/lib/"
+OTHFLAGS="-Wl,-rpath=${MILK_INSTALLDIR}/lib:${BMC_DIR}/lib/"
 
-GCCLINE="gcc createBMCshm.c -o createBMCshm ${IFLAGS} ${LFLAGS} ${lFLAGS} ${OTHFLAGS}"
-
-$GCCLINE
-
-sudo chown root:root ./runBMChex111
-sudo chmod u+s ./runBMChex111
+gcc createBMCshm.c -o createBMCshm ${IFLAGS} ${LFLAGS} ${lFLAGS} ${OTHFLAGS}
